364c1a88b1f96af51c36c19a1ef2c77118cb3946
adrianjustinmartinez@gmail.com <adrianjustinmartinez@gmail.com>
2019-12-09 19:20:48 -0800

    Check

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -5,6 +5,7 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine(TEXT("Welcome to Bull + Cows!"));
+    PrintLine(TEXT("Guess the 4 letter word.")); // Magic number remove!
     PrintLine(TEXT("Press Tab to begin, enter something!"));
 }
 
@@ -12,5 +13,10 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
 {
     ClearScreen();
     FString HiddenWord = TEXT("cake");
-
+    PrintLine(Input);
+    if ( Input == HiddenWord ) {
+        PrintLine("That is the hidden word!");
+    } else {
+        PrintLine("That is wrong, you are dead!");
+    }
 }
