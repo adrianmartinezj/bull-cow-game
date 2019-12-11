03dff32773cf79e151c832d93a0151fdb2352160
adrianjustinmartinez@gmail.com <adrianjustinmartinez@gmail.com>
2019-12-11 14:12:43 -0800

    Simple version

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -47,31 +47,69 @@ void UBullCowCartridge::ProcessGuess(FString Guess)
     {
         PrintLine("You have won!");
         EndGame();
-        return;
     } 
-
-    PrintLine(TEXT("You lost a life!"));
-    --Lives;
-
-    // if ( !IsIsogram )
-    // {
-    //     PrintLine("There are no repeating letters, guess again.");
-    // }
-
-    if (Guess.Len() != HiddenWord.Len()) 
+    else 
     {
-        PrintLine(TEXT("Sorry! Try guessing again.\nYou have %i lives remaining."), Lives);
-        PrintLine(TEXT("The hidden word is %i characters long."), HiddenWord.Len());
+        PrintLine(TEXT("You lost a life!"));
+        --Lives;
+        if (Lives <= 0) 
+        {
+            ClearScreen();
+            PrintLine(TEXT("You have no lives left!"));
+            PrintLine(TEXT("The hidden word was %s."), *HiddenWord);
+            EndGame();
+        }
+        else
+        {
+            IsIsogram(Guess);
+            PrintLine(TEXT("Try guessing again.\nYou have %i lives remaining."), Lives);
+
+        }
     }
     
-    if (Lives <= 0) 
-    {
-        ClearScreen();
-        PrintLine(TEXT("You have no lives left!"));
-        PrintLine(TEXT("The hidden word was %s."), *HiddenWord);
-        EndGame();
-        return;
-    }
+}
 
-    PrintLine(TEXT("Guess again! You have %i lives left."), Lives);
+void UBullCowCartridge::IsIsogram(FString Word)
+{
+    if (Word.Len() != HiddenWord.Len()) 
+    {
+        PrintLine("There are no repeating letters.");
+        PrintLine(TEXT("The hidden word is %i characters long."), HiddenWord.Len());
+    }
+    else
+    {
+        int32 bulls = 0;
+        int32 cows = 0;
+
+        // Bull check
+        for ( int32 i = 0; i < Word.Len(); i++ )
+        {
+            if (Word[i] == HiddenWord[i])
+            {
+                bulls++;
+            }
+
+            FString remainingLetters;
+
+            for ( int32 j = i; j < HiddenWord.Len(); j++)
+            {
+                remainingLetters.AppendChar(HiddenWord[j]);
+            }
+
+            if (remainingLetters.Contains(&Word[i]))
+            {
+                cows++;
+            }
+        }
+
+        if ( bulls > 0)
+        {
+            PrintLine(TEXT("You have %i bulls."), bulls);
+        }
+
+        if ( cows > 0 )
+        {
+            PrintLine(TEXT("You have %i cows."), cows);
+        }
+    }
 }
